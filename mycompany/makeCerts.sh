340479f17506d5cd27685b472fdaae357a9e2e38
Gary Archer <32388530+gary-archer@users.noreply.github.com>
2022-05-18 21:07:38 +0100

    Update makeCerts.sh

--- mycompany/makeCerts.sh
+++ mycompany/makeCerts.sh
@@ -42,7 +42,7 @@ WILDCARD_DOMAIN_NAME="*.$ORGANIZATION.com"
 #
 # Create the root public + private key
 #
-#openssl genrsa -out $ROOT_CERT_FILE_PREFIX.key 2048
+openssl genrsa -out $ROOT_CERT_FILE_PREFIX.key 2048
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the Root CA key'
   exit 1
@@ -51,17 +51,17 @@ fi
 #
 # Create the root certificate file, which has a long lifetime
 #
-#openssl req \
-#    -x509 \
-#    -new \
-#    -nodes \
-#    -key $ROOT_CERT_FILE_PREFIX.key \
-#    -out $ROOT_CERT_FILE_PREFIX.pem \
-#    -subj "/CN=$ROOT_CERT_DESCRIPTION" \
-#    -reqexts v3_req \
-#    -extensions v3_ca \
-#    -sha256 \
-#    -days 3650
+openssl req \
+    -x509 \
+    -new \
+    -nodes \
+    -key $ROOT_CERT_FILE_PREFIX.key \
+    -out $ROOT_CERT_FILE_PREFIX.pem \
+    -subj "/CN=$ROOT_CERT_DESCRIPTION" \
+    -reqexts v3_req \
+    -extensions v3_ca \
+    -sha256 \
+    -days 3650
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the Root CA'
   exit 1
