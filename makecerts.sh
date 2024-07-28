3f6fa26a9400b4c90eb019a28d03b4f49ee702d5
gary-archer <garyarcher36@gmail.com>
2024-07-28 11:40:56 +0100

    Updates

--- makecerts.sh
+++ makecerts.sh
@@ -32,7 +32,7 @@ case "$(uname -s)" in
 esac
 
 #
-# Require OpenSSL 3 to create P12 files
+# Require OpenSSL 3 so that up to date syntax can be used
 #
 OPENSSL_VERSION_3=$(openssl version | grep 'OpenSSL 3')
 if [ "$OPENSSL_VERSION_3" == '' ]; then
@@ -54,14 +54,10 @@ fi
 cd "$BASEDOMAIN"
 
 #
-# Root certificate parameters
+# Parameters that vary between environments
 #
 ROOT_CERT_FILE_PREFIX="$BASEDOMAIN.ca"
 ROOT_CERT_DESCRIPTION="Development CA for $BASEDOMAIN.com"
-
-#
-# SSL certificate parameters
-#
 SSL_CERT_FILE_PREFIX="$BASEDOMAIN.ssl"
 SSL_CERT_PASSWORD='Password1'
 WILDCARD_DOMAIN_NAME="*.$BASEDOMAIN.com"
@@ -82,7 +78,7 @@ openssl req \
     -x509 \
     -new \
     -key $ROOT_CERT_FILE_PREFIX.key \
-    -out $ROOT_CERT_FILE_PREFIX.pem \
+    -out $ROOT_CERT_FILE_PREFIX.crt \
     -subj "/CN=$ROOT_CERT_DESCRIPTION" \
     -addext 'basicConstraints=critical,CA:TRUE' \
     -days 3650
@@ -101,29 +97,20 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# Create the certificate signing request for a wildcard certificate
-#
-openssl req \
-    -new \
-    -key $SSL_CERT_FILE_PREFIX.key \
-    -out $SSL_CERT_FILE_PREFIX.csr \
-    -subj "/CN=$WILDCARD_DOMAIN_NAME" \
-    -addext 'basicConstraints=critical,CA:FALSE'
-if [ $? -ne 0 ]; then
-  echo '*** Problem encountered creating the SSL certificate signing request'
-  exit 1
-fi
-
-#
 # Create the SSL certificate, which must have a limited lifetime
 #
-openssl x509 -req \
-    -in $SSL_CERT_FILE_PREFIX.csr \
-    -CA $ROOT_CERT_FILE_PREFIX.pem \
+openssl req \
+    -x509 \
+    -new \
+    -CA $ROOT_CERT_FILE_PREFIX.crt \
     -CAkey $ROOT_CERT_FILE_PREFIX.key \
-    -out $SSL_CERT_FILE_PREFIX.pem \
+    -key $SSL_CERT_FILE_PREFIX.key \
+    -out $SSL_CERT_FILE_PREFIX.crt \
+    -subj "/CN=*.$SSL_CERT_FILE_PREFIX.com" \
     -days 365 \
-    -extfile server.ext
+    -addext 'basicConstraints=critical,CA:FALSE' \
+    -addext 'extendedKeyUsage=serverAuth' \
+    -addext "subjectAltName=$(cat ./subjectalternativenames.txt)"
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the SSL certificate'
   exit 1
@@ -135,7 +122,7 @@ fi
 openssl pkcs12 \
     -export \
     -inkey $SSL_CERT_FILE_PREFIX.key \
-    -in $SSL_CERT_FILE_PREFIX.pem \
+    -in $SSL_CERT_FILE_PREFIX.crt \
     -name $WILDCARD_DOMAIN_NAME \
     -out $SSL_CERT_FILE_PREFIX.p12 \
     -passout pass:$SSL_CERT_PASSWORD
@@ -145,8 +132,6 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# Delete files no longer needed
+# Indicate success
 #
-rm "$BASEDOMAIN.ca.srl"
-rm "$BASEDOMAIN.ssl.csr"
 echo 'All certificates created successfully'
